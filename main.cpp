efeaa89550dcdbb27c7138ed5146a98591cf444b
pashka <pashka@DESKTOP-31L8IQM>
2024-11-25 12:22:45 +0300

    пиццерия

--- main.cpp
+++ main.cpp
@@ -1,105 +1,142 @@
-#include <iostream>
-#include <Windows.h>
-#include <cstdlib>
-
+#include<iostream>
+#include<Windows.h>
 
 int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	srand(time(NULL));
 
-	const int size = 12;
-	int bot[size]{}, min = 1000, max = 0;
+	int pizza[8]{ 0, 70,80,90,100,110,120, 130 }, drink[6]{ 0,60,70,80,90,100 }, snacks[5]{ 0,50,60,70,80 };
+	int sum= 0, yes = 2;
 
+	std::cout << "\t\tПиццерия пица\n наше меню:\n";
+	std::cout << "7 видов пиццы     \t\t 4 вида закусок\t\t 5 видов напитков\n";
+	std::cout << "1)Римская пицца    \t\t1)Картошка фри\t\t 1)Кола\n";
+	std::cout << "2)Пицца пеперони   \t\t2)Нагетсы     \t\t 2)Фанта\n";
+	std::cout << "3)Итальянская пицца\t\t3)марковные палолчки\t 3)Спрайт\n";
+	std::cout << "4)Метровая пицца   \t\t4)Картошка по деревенски 4)Сок добрый\n";
+	std::cout << "5)Имбовая пицца    \t\t		\t 5)Имба\n";
+	std::cout << "6)Домашняя пицца\n";
 
-	std::cout << "задание 1\n";
-
-	for (int i = 0; i < size; i++)
+	while (true)
 	{
-		bot[i] = rand() % 1000;
-		std::cout << bot[i] << " ";
-	}
-	std::cout << "\n";
+		std::cout << "введите номер пиццы которую вы хотите взять(0 - не хотите брать): ";
+		int number_p;
+		std::cin >> number_p;
+		int calp = 0;
+		while (true)
+		{
+			if (number_p == 0)
+			{
+				break;
+			}
+			std::cout << "какое количество вы хотите взять? ";
+			int quantity;
+			std::cin >> quantity;
+			sum += pizza[number_p] * quantity;
+			calp += quantity;
+			std::cout << "вы хотите взять еще пиццы? Напишите 1 если да 2 если нет. ";
+			std::cin >> yes;
+			if (number_p > 7)
+			{
+				std::cout << "error";
+			}
+			if (yes == 1)
+			{
+				std::cout << "введите номер пиццы которую вы хотите взять: ";
+				std::cin >> number_p;
+				continue;
+			}
+			if (yes == 2)
+			{
+				break;
+			}
+		}
+		sum -= 70 * (calp / 5);
 
-	for (int i = 0; i < 10; i++)
+		std::cout << "введите номер закуски которую вы хотите взять: ";
+		int number_z;
+		std::cin >> number_z;
+		int cal_z;
+		for (int i = 0; i < 100; i++)
+		{
+			if (number_z == 0)
+			{
+				break;
+			}
+			std::cout << "какое количество вы хотите взять? ";
+			int quantity1;
+			std::cin >> quantity1;
+			sum += snacks[number_z] * quantity1;
+			std::cout << "вы хотите взять еще закуски? Напишите 1 если да 2 если нет. ";
+			std::cin >> yes;
+			if (number_z > 4)
+			{
+				std::cout << "error";
+			}
+			if (yes == 1)
+			{
+				std::cout << "введите номер закуски которую вы хотите взять: ";
+				std::cin >> number_z;
+				continue;
+			}
+			if (yes == 2)
+			{
+				break;
+			}
+
+		}
+
+		std::cout << "введите номер напитка который вы хотите взять: ";
+		int number_w;
+		std::cin >> number_w;
+		int cal_w;
+		for (int i = 0; i < 100; i++)
+		{
+			if (number_w == 0)
+			{
+				break;
+			}
+			std::cout << "какое количество вы хотите взять? ";
+			int quantity2;
+			std::cin >> quantity2;
+			sum += drink[number_w] * quantity2;
+			std::cout << "вы хотите еше взять напиток? Напишите 1 если да 2 если нет. ";
+			std::cin >> yes;
+			if (number_w > 5)
+			{
+				std::cout << "error";
+			}
+			if (number_w == 5)
+			{
+				sum -= 100 * (quantity2 / 4);
+			}
+			if (yes == 1)
+			{
+				std::cout << "введите номер напитка который вы хотите взять: ";
+				std::cin >> number_w;
+				continue;
+			}
+			if (yes == 2)
+			{
+				break;
+			}
+			
+		}
+		break;
+
+	}
+	if (sum > 999)
 	{
-		if (bot[i] > max)
-		{
-			max = bot[i];
-		}
+		sum = sum - (sum / 100 * 15);
+		std::cout << "ваш заказ составил: " << sum << "р";
 	}
-	std::cout << "максимальное число: " << max;
-
-	for (int i = 0; i < 10; i++)
+	if (sum < 1000)
 	{
-		if (bot[i] < min)
-		{
-			min = bot[i];
-		}
+		std::cout << "ваш заказ составил: " << sum << "р";
 	}
-	std::cout << "\nминимальное число: " << min;
-
-
-	int  summa = 0, user;
-
-	std::cout << "\nзадание 2\n";
-	std::cout << "введите число диапазона от 0 до 1000(использется массив из первого задания): ";
-	std::cin >> user;
-
-	for (int i = 0; i < size; i++)
-	{
-		if (user > bot[i])
-		{
-			summa += bot[i];
-		}
-	}
-	std::cout << "сумма равна " << summa;
-
-
-
-	std::cout << "\nзадание 3\n";
-	std::cout << "введите прибыль за каждый месяц: ";
-	int const SIZE = 12;
-	int money[SIZE]{};
-	for (int i = 0; i < SIZE; i++)
-	{
-		std::cin >> money[i];
-	}
-
-	std::cout << "введите  диапазон месяцев\nначало: ";
-	int beginning;
-	std::cin >> beginning;
-	std::cout << "конец: ";
-	int end;
-	std::cin >> end;
-	int big=0, maloe=10000000;
-	int month1, month2;
-
-	for (int i = beginning; i < end; i++)
-	{
-		if (money[i] > big)
-		{
-			big = money[i];	
-		}
-		else if (money[i] < maloe)
-		{
-			maloe = money[i];
-		}
-	}
-	for (int i = 0; i < SIZE; i++)
-	{
-		if (money[i] == big)
-		{
-			month1 = i + 1;
-		}
-		else if (money[i] == maloe)
-		{
-			month2 = i + 1;
-		}
-	}
-	std::cout << month1 << " самый прибыльный месяц " << big << "р\n";
-	std::cout << month2 << " самый не прибыльный месяц " << maloe << "р";
 
 	return 0;
 }
+
+
